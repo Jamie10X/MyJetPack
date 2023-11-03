cb407ae1fb3988644c0a19ab394770295eea302c
Jamie10X <jamshidboynazarov0@gmail.com>
2023-11-03 15:07:09 +0900

    Initial commit

--- app/src/main/java/com/example/myjetpack/MainActivity.kt
+++ app/src/main/java/com/example/myjetpack/MainActivity.kt
@@ -3,44 +3,44 @@ package com.example.myjetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myjetpack.ui.theme.MyJetPackTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.myjetpack.bottom_navigation.Home
+import com.example.myjetpack.bottom_navigation.HomeScreen
+import com.example.myjetpack.bottom_navigation.MyBottomNavigation
+import com.example.myjetpack.bottom_navigation.Settings
+import com.example.myjetpack.bottom_navigation.SettingsScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyJetPackTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyApp()
         }
     }
 }
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MyApp(){
+    val navController = rememberNavController()
+    Scaffold (bottomBar = { MyBottomNavigation(navController = navController)}) {
+   Box (Modifier.padding(it)){
+       NavHost(navController = navController, startDestination = Home.route) {
+           composable(Home.route){
+               HomeScreen()
+           }
+           composable(Settings.route){
+               SettingsScreen()
+           }
+       }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyJetPackTheme {
-        Greeting("Android")
+   }
     }
 }
